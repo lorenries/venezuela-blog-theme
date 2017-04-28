f6affa377844fea7c748b78280b49b9dae50260b
Loren Riesenfeld <Loren@iMac-2.local>
2017-04-28 17:27:56 -0400

    add header image

--- header.php
+++ header.php
@@ -27,9 +27,9 @@
 		<div class="site-branding">
 			<?php
 			if ( is_front_page() && is_home() ) : ?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<h1 class="site-title white"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="link white"><?php bloginfo( 'name' ); ?></a></h1>
 			<?php else : ?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<p class="site-title white"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" class="white"><?php bloginfo( 'name' ); ?></a></p>
 			<?php
 			endif;
 
@@ -44,6 +44,13 @@
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'venezuela-blog' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'menu-1', 'menu_id' => 'primary-menu' ) ); ?>
 		</nav><!-- #site-navigation -->
+		
 	</header><!-- #masthead -->
+
+	<!-- header image -->
+	<div class="w-100 h-auto">
+	<img src="<?php header_image(); ?>" class="w-100 h-25"></img>
+	</div>
+	<!-- end header image -->
 
 	<div id="content" class="site-content">
