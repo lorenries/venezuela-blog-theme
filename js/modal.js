b25431a581c8f9884880890c48b5a7f94ff7d378
Loren Riesenfeld <Loren@iMac-2.local>
2017-05-03 17:15:45 -0400

    style modal

---
+++ js/modal.js
@@ -0,0 +1,37 @@
+(function() {
+    document.addEventListener("DOMContentLoaded", function(event) {
+    var link = document.querySelector("a[href='#about']"),
+        close = document.querySelector(".closebtn"),
+        body = document.querySelector("body"),
+        overlay = document.querySelector( '.overlay' );
+
+    link.addEventListener("click", toggleModal);
+    close.addEventListener("click", toggleModal);
+
+    
+    // if (overlay.classList.contains("open")) {
+    // }
+
+function keypress(event) {
+    console.log(event.key);
+    if (event.key == 'Escape') {
+        overlay.classList.remove("open");
+        body.classList.remove("overflow-hidden");
+        document.removeEventListener("keyup", keypress);
+    }
+}     
+
+    function toggleModal() {
+        if (overlay.classList.contains("open")) {
+            overlay.classList.remove("open");
+            body.classList.remove("overflow-hidden");
+        } else {
+            overlay.classList.add("open");
+            body.classList.add("overflow-hidden");
+            document.addEventListener("keyup", keypress);
+        }
+    }
+  });
+
+})();
+
