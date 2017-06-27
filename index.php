c3afef9a59e30c9e67456ae1b257e586b5500f0a
lorenries <lorenries@users.noreply.github.com>
2017-06-27 17:36:24 -0400

    add sidebar using flexbox

--- index.php
+++ index.php
@@ -15,35 +15,42 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main mw9 center" role="main">
+		<main id="main" class="site-main mw8-5 center" role="main">
 
 			<?php if ( have_posts() ) : ?>
+				
+				<div class="flex flex-column flex-row-l justify-between-l justify-center">
 
+					<div class="" style="flex-basis: 70%">
 
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
+						<?php /* Start the Loop */ ?>
+						<?php while ( have_posts() ) : the_post(); ?>
 
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to overload this in a child theme then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						get_template_part( 'template-parts/content', get_post_format() );
-					?>
+							<?php
+								/* Include the Post-Format-specific template for the content.
+								 * If you want to overload this in a child theme then include a file
+								 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+								 */
+								get_template_part( 'template-parts/content', get_post_format() );
+							?>
 
-				<?php endwhile; ?>
+						<?php endwhile; ?>
 
-				<?php the_posts_navigation(); ?>
+						<?php the_posts_navigation(); ?>
 
-			<?php elseif ( current_user_can( 'edit_posts' ) ) : ?>
+					<?php elseif ( current_user_can( 'edit_posts' ) ) : ?>
 
-				<?php get_template_part( 'no-results', 'index' ); ?>
+						<?php get_template_part( 'no-results', 'index' ); ?>
 
-			<?php endif; ?>
+					<?php endif; ?>
+
+					</div>
+				<div style="flex-basis: 30%"><?php get_sidebar(); ?></div>
+			</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+
 get_footer();
