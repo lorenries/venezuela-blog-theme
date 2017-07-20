ce50b167fd73d3a00f9980009550c6b38bb11b74
Loren Riesenfeld <lries@Lorens-MacBook-Pro.local>
2017-07-20 17:57:28 -0400

    add social links to header

--- header.php
+++ header.php
@@ -59,22 +59,22 @@
 									</a>
 								</li>
 								<li class="flex items-center">
-									<a class="link dim white dib h1-5 w1-5 mr3 self-center" href="#" title="">
-										<svg data-icon="twitter" viewBox="0 0 32 32" style="fill:currentColor">
+									<a class="link hover-twitter white dib h1-5 w1-5 mr3 self-center" href="https://twitter.com/WOLA_org" title="Follow WOLA on Twitter" rel="noopener noreferrer">
+										<svg data-icon="twitter" viewBox="0 0 32 32" style="fill:currentColor" target="_blank">
 											<title>twitter icon</title>
 											<path d="M2 4 C6 8 10 12 15 11 A6 6 0 0 1 22 4 A6 6 0 0 1 26 6 A8 8 0 0 0 31 4 A8 8 0 0 1 28 8 A8 8 0 0 0 32 7 A8 8 0 0 1 28 11 A18 18 0 0 1 10 30 A18 18 0 0 1 0 27 A12 12 0 0 0 8 24 A8 8 0 0 1 3 20 A8 8 0 0 0 6 19.5 A8 8 0 0 1 0 12 A8 8 0 0 0 3 13 A8 8 0 0 1 2 4"></path>
 										</svg>
 									</a>
 								</li>
 								<li class="flex items-center">
-									<a class="link dim white dib h1-5 w1-5 mr3 self-center" href="#" title="">
+									<a class="link hover-facebook white dib h1-5 w1-5 mr3 self-center" href="https://www.facebook.com/WOLA.org/" title="Follow WOLA on Facebook" rel="noopener noreferrer" target="_blank">
 										<svg data-icon="facebook" viewBox="0 0 32 32" style="fill:currentColor">
 											<title>facebook icon</title>
 											<path d="M8 12 L13 12 L13 8 C13 2 17 1 24 2 L24 7 C20 7 19 7 19 10 L19 12 L24 12 L23 18 L19 18 L19 30 L13 30 L13 18 L8 18 z"></path>
 										</svg>
 									</a>
 								</li>
-								<li class="flex items-center"><a href="https://wola.org" class="link white dim">WOLA.org</a></li>
+								<li class="flex items-center"><a href="https://wola.org" class="link white hover-wola-blue" rel="noopener noreferrer" target="_blank">WOLA.org</a></li>
 							</ul>
 						</div>
 
