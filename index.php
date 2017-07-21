94bc706bc48c2e2e973409d0e612d36f68fbf2cf
lorenries <lorenries@users.noreply.github.com>
2017-07-21 16:37:56 -0400

    not sure

--- index.php
+++ index.php
@@ -14,43 +14,46 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main mw8 center" role="main">
+<div id="primary" class="content-area">
+	<main id="main" class="site-main mw8 center" role="main">
 
-			<?php if ( have_posts() ) : ?>
-				
-				<div class="flex flex-column flex-row-l justify-center">
+		<?php if ( have_posts() ) : ?>
 
-					<div class="center self-center mw-100 w-70-l">
+			<div class="flex flex-column flex-row-l justify-center">
 
-						<?php /* Start the Loop */ ?>
-						<?php while ( have_posts() ) : the_post(); ?>
+				<div class="center self-center mw-100 w-70-l">
 
-							<?php
+					<?php /* Start the Loop */ ?>
+					<?php while ( have_posts() ) : the_post(); ?>
+
+						<?php
 								/* Include the Post-Format-specific template for the content.
 								 * If you want to overload this in a child theme then include a file
 								 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 								 */
 								get_template_part( 'template-parts/content', get_post_format() );
-							?>
+								?>
 
-						<?php endwhile; ?>
+							<?php endwhile; ?>
 
-						<?php the_posts_navigation(); ?>
+							<div class="flex justify-between pa3">
+								<div class=""><?php next_posts_link('&larr; Older Posts') ?></div>
+								<div class=""><?php previous_posts_link('Newer Posts &rarr;') ?></div>
+							</div>
 
-					<?php elseif ( current_user_can( 'edit_posts' ) ) : ?>
+						<?php elseif ( current_user_can( 'edit_posts' ) ) : ?>
 
-						<?php get_template_part( 'no-results', 'index' ); ?>
+							<?php get_template_part( 'no-results', 'index' ); ?>
 
-					<?php endif; ?>
+						<?php endif; ?>
 
 					</div>
-				<div class="center w-100 w-30-l"><?php get_sidebar(); ?></div>
-			</div>
+					<div class="center w-100 w-30-l pb4 pb0-l"><?php get_sidebar(); ?></div>
+				</div>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+			</main><!-- #main -->
+		</div><!-- #primary -->
 
-<?php
+		<?php
 
-get_footer();
+		get_footer();
