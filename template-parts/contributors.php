6b20061e9cccf0aee37f8afd2f4e68524966d98c
lorenries <lorenries@users.noreply.github.com>
2017-07-27 12:09:25 -0400

    fix relative urls in davids link

--- template-parts/contributors.php
+++ template-parts/contributors.php
@@ -18,10 +18,10 @@
 
 		<div class="flex flex-column flex-row-ns justify-start items-center">
 			<div class="order-1 order-0-ns pt4 pt0-ns w-third">
-				<a href="author/david-smilde/" target="_blank" rel="noopener noreferrer"><img class="mw4 br-100 w-100 h-auto" src="wp-content/uploads/2017/07/DS-Thumb.jpg" alt="David Smilde"></a>
+				<a href="author/david-smilde/" target="_blank" rel="noopener noreferrer"><img class="mw4 br-100 w-100 h-auto" src="/wp-content/uploads/2017/07/DS-Thumb.jpg" alt="David Smilde"></a>
 			</div>
 			<div class="pl0 pl4-ns order-0 order-1-ns w-two-thirds">
-				<h1 class="f5 ma0 pb1"><a class="wola-gray hover-wola-blue link" href="author/david-smilde/" target="_blank" rel="noopener noreferrer">David Smilde</a></h1>
+				<h1 class="f5 ma0 pb1"><a class="wola-gray hover-wola-blue link" href="/author/david-smilde/" target="_blank" rel="noopener noreferrer">David Smilde</a></h1>
 				<p class="f6 ma0 fw2">WOLA Senior Fellow and the Charles A. and Leo M. Favrot Professor of Human Relations at Tulane University</p>
 			</div>
 
