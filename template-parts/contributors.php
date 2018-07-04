24a7e8497e39836f856705b2894ce859c84afa1e
Loren Riesenfeld <lorenries@users.noreply.github.com>
2018-07-03 21:21:56 -0400

    add hugo to sidebar

--- template-parts/contributors.php
+++ template-parts/contributors.php
@@ -9,16 +9,16 @@
 
 ?>
 
-<section class="mw6 center">
+<aside class="mw6 center">
 	<header class="page-header">
 		<h2 class="system fw2 bb b--mid-gray mb3 f3">Contributors</h1>
 	</header><!-- .page-header -->
 
 	<div class="page-content">
 
-		<div class="flex flex-column flex-row-ns justify-start items-center">
+		<div class="flex flex-column flex-row-ns justify-start items-center pv2">
 			<div class="order-1 order-0-ns pt4 pt0-ns w-third">
-				<a href="author/david-smilde/" target="_blank" rel="noopener noreferrer"><img class="mw4 br-100 w-100 h-auto" src="/wp-content/uploads/2017/07/DS-Thumb.jpg" alt="David Smilde"></a>
+				<a href="/author/david-smilde/" target="_blank" rel="noopener noreferrer"><img class="mw4 br-100 w-100 h-auto" src="/wp-content/uploads/2017/07/DS-Thumb.jpg" alt="David Smilde"></a>
 			</div>
 			<div class="pl0 pl4-ns order-0 order-1-ns w-two-thirds">
 				<h1 class="f5 ma0 pb1"><a class="wola-gray hover-wola-blue link" href="/author/david-smilde/" target="_blank" rel="noopener noreferrer">David Smilde</a></h1>
@@ -27,16 +27,25 @@
 
 		</div>
 
-		<div class="flex flex-column flex-row-ns justify-start items-center">
+		<div class="flex flex-column flex-row-ns justify-start items-center pv2">
 			<div class="order-1 order-0-ns pt4 pt0-ns w-third">
-				<a href="author/geoff-ramsey/" rel="noopener noreferrer"><img class="mw4 br-100 w-100 h-auto" src="/wp-content/uploads/2017/07/GR-Thumb.jpg" alt="Geoff Ramsey"></a>
+				<a href="/author/geoff-ramsey/" rel="noopener noreferrer"><img class="mw4 br-100 w-100 h-auto" src="/wp-content/uploads/2017/07/GR-Thumb.jpg" alt="Geoff Ramsey"></a>
 			</div>
 			<div class="pl0 pl4-ns order-0 order-1-ns w-two-thirds">
 				<h1 class="f5 ma0 pb1"><a class="wola-gray hover-wola-blue link" href="author/geoff-ramsey/" rel="noopener noreferrer">Geoff Ramsey</a></h1>
 				<p class="f6 ma0 fw2">WOLA Assistant Director for Venezuela</p>
 			</div>
-
+		</div>
+		
+		<div class="flex flex-column flex-row-ns justify-start items-center pv2">
+			<div class="order-1 order-0-ns pt4 pt0-ns w-third">
+				<a href="/author/hugo-perez-hernaiz/" rel="noopener noreferrer"><img class="mw4 br-100 w-100 h-auto" src="/wp-content/uploads/2018/07/HP-Thumb.jpg" alt="Hugo Pérez Hernáiz"></a>
+			</div>
+			<div class="pl0 pl4-ns order-0 order-1-ns w-two-thirds">
+				<h1 class="f5 ma0 pb1"><a class="wola-gray hover-wola-blue link" href="author/geoff-ramsey/" rel="noopener noreferrer">Hugo Pérez Hernáiz</a></h1>
+				<p class="f6 ma0 fw2">Venezuelan Sociologist and Translator</p>
+			</div>
 		</div>
 
 	</div><!-- .page-content -->
-</section><!-- .no-results -->
+</aside><!-- .no-results -->
